9d861cb9a9eb7c42c733516b0912f2fc021011f4
geekychandan <get2chandanmohapatra@gmail.com>
2024-04-29 13:09:35 +0530

    Updated README with new information

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,9 @@ module.exports = {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
         },
+        custom: {
+          'neon': '#55dd4a'
+        },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
